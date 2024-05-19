1186758c0392e167524f6bd2dd4cba9af07189bb
blue32a <blue32a.jp@gmail.com>
2024-05-19 13:04:53 +0000

    リスト内の最大値を見つけるステートレスプロパティ

--- test/pbt_test.exs
+++ test/pbt_test.exs
@@ -8,8 +8,30 @@ defmodule PbtTest do
     end
   end
 
+  property "最大の要素を見つける" do
+    forall x <- non_empty(list(integer())) do
+      biggest(x) == List.last(Enum.sort(x))
+    end
+  end
+
   def boolean(_) do
     true
+  end
+
+  def biggest([head | tail]) do
+    biggest(tail, head)
+  end
+
+  defp biggest([], max) do
+    max
+  end
+
+  defp biggest([head | tail], max) when head >= max do
+    biggest(tail, head)
+  end
+
+  defp biggest([head | tail], max) when head < max do
+    biggest(tail, max)
   end
 
   def my_type() do
